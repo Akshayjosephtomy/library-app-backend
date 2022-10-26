af83291f668bcd5189e8e9aafa5ee3eb3074c088
Akshayjosephtomy <akshayjt007@gmail.com>
2022-10-27 00:26:13 +0530

    l

--- src/main/java/com/nestdigital/libraryappbackend/controller/LibraryController.java
+++ src/main/java/com/nestdigital/libraryappbackend/controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.nestdigital.libraryappbackend.model.LibraryModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -25,5 +26,27 @@ public class LibraryController {
     @GetMapping("/viewbooks")
     public List<LibraryModel> viewbooks(){
         return (List<LibraryModel>) dao.findAll();
+    }
+
+    @CrossOrigin(origins = "*")
+    @Transactional
+    @PostMapping(path = "/deletebook",consumes = "application/json",produces = "application/json")
+    public String deletebook(@RequestBody LibraryModel library){
+        dao.deleteBookById(library.getId());
+        return "(status:'success')";
+    }
+
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/searchbook",consumes = "application/json",produces = "application/json")
+    public List<LibraryModel> searchbook(@RequestBody LibraryModel book){
+        return (List<LibraryModel>) dao.searchBook(book.getName());
+    }
+
+    @CrossOrigin(origins = "*")
+    @Transactional
+    @PostMapping(path = "/updatebook",consumes = "application/json",produces = "application/json")
+    public String updatebook(@RequestBody LibraryModel library){
+        dao.updatBook(library.getName(), library.getAuthor(), library.getPdate(), library.getPrice(), library.getId());
+        return "(status:'success')";
     }
 }
--- src/main/java/com/nestdigital/libraryappbackend/dao/LibraryDao.java
+++ src/main/java/com/nestdigital/libraryappbackend/dao/LibraryDao.java
@@ -1,7 +1,22 @@
 package com.nestdigital.libraryappbackend.dao;
 
 import com.nestdigital.libraryappbackend.model.LibraryModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LibraryDao extends CrudRepository<LibraryModel,Integer> {
+
+    @Modifying
+    @Query(value = "DELETE FROM `books` WHERE `id`= :id",nativeQuery = true)
+    void deleteBookById(Integer id);
+
+    @Query(value = "SELECT `id`, `author`, `name`, `pdate`, `price` FROM `books` WHERE `name`= :name",nativeQuery = true)
+    List<LibraryModel> searchBook(String name);
+
+    @Modifying
+    @Query(value = "UPDATE `books` SET `author`= :author,`name`= :name,`pdate`= :pdate,`price`= :price WHERE `id`= :id",nativeQuery = true)
+    void updatBook(String name,String author,String pdate,String price,Integer id);
 }
